dc62e118f1a4061dd3ffb7aa74a7fa24501c2950
cmy <1732728869@qq.com>
2019-12-29 20:55:06 +0800

    完成图片渲染

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -6,19 +6,23 @@
       </div>
       <!--        按钮区域-->
       <div class="btn_box">
-      <el-radio-group v-model="filterParams.collect" size="small">
-        <el-radio-button :label="false">全部</el-radio-button>
-        <el-radio-button :label="true">收藏</el-radio-button>
-      </el-radio-group>
+        <el-radio-group v-model="filterParams.collect" size="small">
+          <el-radio-button :label="false">全部</el-radio-button>
+          <el-radio-button :label="true">收藏</el-radio-button>
+        </el-radio-group>
         <el-button style="float:right" type="success" size="small">添加素材</el-button>
 
       </div>
       <div class="img_list">
-        <div class="img_item" v-for="index in 10" :key="index">
-          <img src="../../assets/images/avatar.jpg" alt="">
+        <div class="img_item" v-for="item in images" :key="item.id">
+          <img :src="item.url" alt/>
+          <div class="option">
+            <span class="el-icon-star-off" :class="{red:item.is_collected}"></span>
+            <span class="el-icon-delete"></span>
+          </div>
         </div>
       </div>
-<!--      分页-->
+      <!--      分页-->
       <el-pagination
         background
         layout="prev, pager, next"
@@ -34,8 +38,25 @@
     data() {
       return {
         filterParams: {
-          collect: false
-        }
+          collect: false,
+          //默认显示第一页
+          page: 1,
+          per_page: 10
+        },
+        //图片数据
+        images: []
+      }
+    },
+    //页面初始化的时候加载，请求数据
+    created() {
+      this.getImages()
+    },
+    methods: {
+      async getImages() {
+        const {data: {data}} = await this.$http.get('/user/images',
+          {params: this.filterParams})
+        this.images = data.results
+        console.log(this.images)
       }
     }
   }
@@ -44,6 +65,7 @@
 <style scoped lang="less">
   .img_list {
     margin-top: 20px;
+
     .img_item {
       width: 262px;
       height: 250px;
